cd02a46926cddc126c88f57b675e033fc988744c
Craigsuter <willcrane78@gmail.com>
2021-08-27 23:13:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,21 +6,10 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="discordpy-replit-heroku",
     version="0.4.0",
-    author="John Doe",
-    author_email="johndo3@repl.email",
-    description="Hosting your repl based discord.py bot on Heroku to keep it running INDEFINETLY",
+    author="cool story bob",
+    description="Hosting a bot on Discord.py",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/syntax-corp/discordpy-replit-heroku",
-    project_urls={
-        "Issue tracker": "https://github.com/syntax-corp/discordpy-replit-heroku/issues",
-    },
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     install_requires=[
         "discord.py",
         "flask",
